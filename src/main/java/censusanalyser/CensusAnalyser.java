bf52a9832f8894a25d38c097dd40d523dad2c31a
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-14 15:33:41 +0530

    Add : function to load india  state code

--- src/main/java/censusanalyser/CensusAnalyser.java
+++ src/main/java/censusanalyser/CensusAnalyser.java
@@ -8,7 +8,6 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
-import java.util.concurrent.Callable;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
@@ -18,11 +17,10 @@ public class CensusAnalyser {
             csvToBeanBuilder.withType(IndiaCensusCSV.class);
             csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
             CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
-            Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();;
-            Iterable<IndiaCensusCSV> csvIterable = () -> censusCSVIterator;
+            Iterator<IndiaCensusCSV>  stateCSVIterator = csvToBean.iterator();;
+            Iterable<IndiaCensusCSV> csvIterable = () -> stateCSVIterator;
             int namOfRecords = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
             return namOfRecords;
-
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                          CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -31,5 +29,25 @@ public class CensusAnalyser {
                          CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
         }
     }
+
+    public int loadIndiaStateCode(String csvFilePath) throws CensusAnalyserException {
+        try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+            CsvToBeanBuilder<IndiaStateCodeCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+            csvToBeanBuilder.withType(IndiaStateCodeCSV.class);
+            csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+            CsvToBean<IndiaStateCodeCSV> csvToBean = csvToBeanBuilder.build();
+            Iterator<IndiaStateCodeCSV> statCodeCSVIterator = csvToBean.iterator();;
+            Iterable<IndiaStateCodeCSV> csvIterable = () -> statCodeCSVIterator;
+            int StateCodeRecords = (int) StreamSupport.stream(csvIterable.spliterator(), false).count();
+            return StateCodeRecords;
+        } catch (IOException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+        } catch (IllegalStateException e) {
+            throw new CensusAnalyserException(e.getMessage(),
+                    CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+        }
+    }
+
 }
 
--- src/main/java/censusanalyser/IndiaStateCodeCSV.java
+++ src/main/java/censusanalyser/IndiaStateCodeCSV.java
@@ -1,4 +1,19 @@
 package censusanalyser;
 
-public class IndiaStateCodeCSv {
+import com.opencsv.bean.CsvBindByName;
+
+public class IndiaStateCodeCSV {
+    @CsvBindByName(column = "State Name", required = true)
+    public String state;
+
+    @CsvBindByName(column = "StateCode", required = true)
+    public String stateCode;
+
+    @Override
+    public String toString() {
+        return "IndiaStateCodeCSV{" +
+                "state='" + state + '\'' +
+                ", stateCode='" + stateCode + '\'' +
+                '}';
+    }
 }
