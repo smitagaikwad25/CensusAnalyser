6be1b0c7328470e7b0a5119a9da1148bb26e524a
smitagaikwad25 <gaikwadsmita021@gmail.com>
2019-12-14 15:32:27 +0530

    Add : give state code csv return exact count

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -8,7 +8,7 @@ public class CensusAnalyserTest {
 
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
-
+    private static final String INDIA_STATE_CODE_CSV_FILE_PATH ="/home/admin1/intelij/CensusAnalyser/CensusAnalyser/src/test/resources/IndiaStateCode.csv";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -30,4 +30,13 @@ public class CensusAnalyserTest {
         }
     }
 
+    @Test
+    public void givenIndianStateCsv_ShouldReturnExactCount() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaStateCode(INDIA_STATE_CODE_CSV_FILE_PATH);
+            Assert.assertEquals(37,numOfRecords);
+        } catch (CensusAnalyserException e) {
+        }
+    }
 }
